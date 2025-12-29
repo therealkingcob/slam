4c7a8d98e3b8f5345256bd23fc360db6cab0058b
Rishi Shah <rishiashah19@gmail.com>
2025-12-28 20:10:01 -0800

    speed optimizations

--- ch6/self/main.cpp
+++ ch6/self/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <opencv2/opencv.hpp>
 #include <cmath>
+#include <chrono>
 
 #include "ORB_pattern.h"
 
@@ -22,7 +23,9 @@ struct feature {
 const double matchingRatio = 0.9;
 const int max_dist = 120;
 
-vector <vector<int>> offsets = {{0 ,4},
+const int goalcols = 640;
+
+const vector <vector<int>> offsets = {{0 ,4},
                                     {1, 3},
                                     {2, 2},
                                     {3, 1},
@@ -58,14 +61,14 @@ vector <vector<int>> offsets = {{0 ,4},
 
                                         
 //gets the brightness as cols x, rows y
-int getBrightness(Mat im, int x, int y) {
+int getBrightness(const Mat& im, int x, int y) {
     uchar b = im.at<uchar>(y,x);
     int brightness = static_cast<int>(b);
     return brightness;
     
 }
 
-double getOrientation(Mat im, int x, int y) {
+double getOrientation(const Mat& im, int x, int y) {
     int r = 15;
 
     // getting all of the pixels in radius 15 circle around the main one
@@ -99,7 +102,7 @@ double getOrientation(Mat im, int x, int y) {
     return angle;
 }
 
-vector<uint32_t> getDescriptor( Mat gray, int x, int y, feature f)
+vector<uint32_t> getDescriptor(const Mat& gray, int x, int y, feature f)
 {
     vector<uint32_t> desc(8, 0); // 8 × 32 = 256 bits
 
@@ -135,13 +138,37 @@ vector<uint32_t> getDescriptor( Mat gray, int x, int y, feature f)
     return desc;
 }
 
+//to access a certain pixels brightness, we go b[y*img.cols+x]
+vector<int> imgBrightness(const Mat& img) {
+    int x = img.cols;
+    int y = img.rows;
 
-vector<feature> findFeatures(Mat image) {
-    int width = image.cols;
-    int height = image.rows;
+    vector<int> b(x * y);
 
-    Mat gray_image;
-    cvtColor(image, gray_image, COLOR_BGR2GRAY);
+    //fits goes (0,0), (0,1), (0,2) ... (1,0), (1,1) ...
+
+    for(int i = 0; i < y; i++) {
+
+        const uchar* row = img.ptr<uchar>(i);
+
+        for(int j = 0; j < x; j++) {
+            
+            b[i * x + j] = row[j];
+
+        }
+    }
+
+    return b;
+    
+}
+
+
+vector<feature> findFeatures(const Mat& gray_image, const vector<int>& b_img) {
+    int width = gray_image.cols;
+    int height = gray_image.rows;
+
+    //Mat gray_image;
+    //cvtColor(image, gray_image, COLOR_BGR2GRAY);
 
     GaussianBlur(gray_image, gray_image, Size(5,5), 1.0);
 
@@ -149,8 +176,8 @@ vector<feature> findFeatures(Mat image) {
     vector <feature> features;
 
     //Todo: tune these
-    int threshold = 13;
-    int tolerance = 30;
+    int threshold = 15;
+    int tolerance = 40; //the bigger this is, the less features there are
 
     //i is the x-axis
     //j is the y-axis
@@ -158,7 +185,8 @@ vector<feature> findFeatures(Mat image) {
     for (int i = 4; i < width-4; i++) {
         for(int j = 4; j < height-4; j++) {
 
-            int b = getBrightness(gray_image, i, j);
+            int b = b_img[j*width + i];
+            //b[y*img.cols+x]
 
             int brighter = 0;
             int darker = 0;
@@ -166,7 +194,8 @@ vector<feature> findFeatures(Mat image) {
             //we need to go 16 pixels for the entire radius plus n since the series to feature pixel candidates could start on the last pixel of the 16
             for(int f = 0; f < 16 + threshold; f++) {
 
-                int brightnessAtPoint = getBrightness(gray_image, i + offsets[f][0], j - offsets[f][1]);
+                //int brightnessAtPoint = getBrightness(gray_image, i + offsets[f][0], j - offsets[f][1]);
+                int brightnessAtPoint = b_img[((j - offsets[f][1]) * width) + (i + offsets[f][0])];
 
                 if(brightnessAtPoint > b + tolerance) {
 
@@ -208,7 +237,7 @@ vector<feature> findFeatures(Mat image) {
 }
 
 
-Mat drawFeatures(vector<feature> features, Mat image) {
+Mat drawFeatures(const vector<feature>& features, const Mat& image) {
     for(int i = 0; i < features.size(); i++) {
         Point center(features[i].x, features[i].y);
 
@@ -227,7 +256,7 @@ Mat drawFeatures(vector<feature> features, Mat image) {
 
 //calculates the hamming distance between two vectors
 //TODO: Optimize this for speed
-int hammingDistance(vector<uint32_t>a, vector<uint32_t>b) {
+int hammingDistance(const vector<uint32_t>& a, const vector<uint32_t>& b) {
 
     int dist = 0;
 
@@ -251,7 +280,7 @@ int hammingDistance(vector<uint32_t>a, vector<uint32_t>b) {
 //we take the second feature and the entirety of the first feature list and see if the same match occurs both ways
 //if so, thats good, otherwise we remove that match and dont use it
 //uses same first and second as matchFeatures
-bool reverseChecker(vector<feature> first, feature second, int matchedFirst) {
+bool reverseChecker(const vector<feature>& first, const feature& second, int matchedFirst) {
 
     int best_i = -1;
     int best = 1e9;
@@ -297,7 +326,7 @@ bool reverseChecker(vector<feature> first, feature second, int matchedFirst) {
     
 }
 
-vector<pair<feature, feature>> matchFeatures(vector<feature> first, vector<feature> second) {
+vector<pair<feature, feature>> matchFeatures(const vector<feature>& first, const vector<feature>& second) {
     
     //TODO: tune these
     //double ratio = 0.9f;
@@ -320,9 +349,9 @@ vector<pair<feature, feature>> matchFeatures(vector<feature> first, vector<featu
 
             
 
-            if (first[i].descriptors.empty() || second[j].descriptors.empty() || first[i].descriptors.size() != second[j].descriptors.size()) {
-                continue;
-            }
+            //if (first[i].descriptors.empty() || second[j].descriptors.empty() || first[i].descriptors.size() != second[j].descriptors.size()) {
+              //  continue;
+            //}
 
             int dist = hammingDistance(first[i].descriptors, second[j].descriptors);
 
@@ -361,7 +390,7 @@ vector<pair<feature, feature>> matchFeatures(vector<feature> first, vector<featu
 
 
 
-Mat drawMatches(vector<pair<feature, feature>> matches, Mat img, int imgcols) {
+Mat drawMatches(const vector<pair<feature, feature>>& matches, const Mat& img, int imgcols) {
     //draw the lines for matches
 
     Scalar color(255, 0, 0);
@@ -383,10 +412,20 @@ Mat drawMatches(vector<pair<feature, feature>> matches, Mat img, int imgcols) {
 
 int main(int argc, char **argv) {
 
-    Mat image1 = imread("../images/4.png");
-    Mat image2 = imread("../images/3.png");
+    chrono::steady_clock::time_point m1 = chrono::steady_clock::now();
 
-    const int goalcols = 640;
+    chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+
+    Mat image1 = imread("../images/4.png", IMREAD_GRAYSCALE);
+    Mat image2 = imread("../images/3.png", IMREAD_GRAYSCALE);
+
+    chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+
+    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+
+    cout << "Time to read images is: " << time_used.count() << endl;
+
+    t1 = chrono::steady_clock::now();
 
     double fx1 = (double) goalcols / image1.cols;
     double fy1 = fx1;
@@ -394,51 +433,132 @@ int main(int argc, char **argv) {
     double fx2 = (double) goalcols / image1.cols;
     double fy2 = (image1.rows * fy1) / image2.rows;
 
-
     resize(image1, image1, Size(), fx1, fy1, INTER_AREA);
     resize(image2, image2, Size(), fx2, fy2, INTER_AREA);
+
+    t2 = chrono::steady_clock::now();
+
+    time_used = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+
+    cout << "Time to scale images is: " << time_used.count() << endl;
+
 
     cout << "The dimensions of the first image are: " << image1.cols << " x " << image1.rows << endl;
     cout << "The dimensions of the second image are: " << image2.cols << " x " << image2.rows << endl;
 
-    Mat gray_image_1;
-    Mat gray_image_2;
-    
-    cvtColor(image1, gray_image_1, COLOR_BGR2GRAY);
-    cvtColor(image2, gray_image_2, COLOR_BGR2GRAY);
+    t1 = chrono::steady_clock::now();
 
-    vector <feature> features1 = findFeatures(image1);
-    vector <feature> features2 = findFeatures(image2);
+    //Mat gray_image1;
+    //Mat gray_image2;
+    
+    //cvtColor(image1, gray_image1, COLOR_BGR2GRAY);
+    //cvtColor(image2, gray_image2, COLOR_BGR2GRAY);
+
+    t2 = chrono::steady_clock::now();
+
+    time_used = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+
+    cout << "Time to make images gray is: " << time_used.count() << endl;
+
+    t1 = chrono::steady_clock::now();
+
+    vector<int> gray_image1_b = imgBrightness(image1);
+    vector<int> gray_image2_b = imgBrightness(image2);
+
+    t2 = chrono::steady_clock::now();
+
+    time_used = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+
+    cout << "Time to get the entire brightness is: " << time_used.count() << endl;
+
+    t1 = chrono::steady_clock::now();
+
+    vector <feature> features1 = findFeatures(image1, gray_image1_b);
+    vector <feature> features2 = findFeatures(image2, gray_image2_b);
+
+     t2 = chrono::steady_clock::now();
+
+    time_used = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+
+    cout << "Time to find features is: " << time_used.count() << endl;
 
     cout << "The number of features of the first image is: " << features1.size() << endl;
     cout << "The number of features of the second image is: " << features2.size() << endl;
 
+     t1 = chrono::steady_clock::now();
+
     for(int i = 0; i < features1.size(); i++) {
-        features1[i].descriptors = getDescriptor(gray_image_1, features1[i].x, features1[i].y, features1[i]);
+        features1[i].descriptors = getDescriptor(image1, features1[i].x, features1[i].y, features1[i]);
     } 
+
+    for(int i = 0; i < features2.size(); i++) {
+        features2[i].descriptors = getDescriptor(image2, features2[i].x, features2[i].y, features2[i]);
+    }
+
+     t2 = chrono::steady_clock::now();
+
+    time_used = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+
+    cout << "Time to find descriptors is: " << time_used.count() << endl;
+
+     t1 = chrono::steady_clock::now();
 
     //deletes features with empty descriptors
     features1.erase(remove_if(features1.begin(), features1.end(), [](const feature& f) {return f.descriptors.size() != 8;}),features1.end());
-
-    for(int i = 0; i < features2.size(); i++) {
-        features2[i].descriptors = getDescriptor(gray_image_2, features2[i].x, features2[i].y, features2[i]);
-    }
-
     features2.erase(remove_if(features2.begin(), features2.end(), [](const feature& f) {return f.descriptors.size() != 8;}),features2.end());
+
+     t2 = chrono::steady_clock::now();
+
+     time_used = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+
+    cout << "Time to remove bad descriptors is: " << time_used.count() << endl;
+
+
+
+     t1 = chrono::steady_clock::now();
+
 
     vector<pair<feature, feature>> matches = matchFeatures(features1, features2);
 
+     t2 = chrono::steady_clock::now();
+
+     time_used = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+
+    cout << "Time to match features is: " << time_used.count() << endl;
+
     cout << "The number of matches is: " << matches.size() << endl;
+
+     t1 = chrono::steady_clock::now();
 
     image1 = drawFeatures(features1, image1);
     image2 = drawFeatures(features2, image2);
+
+     t2 = chrono::steady_clock::now();
+
+     time_used = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+
+    cout << "Time to draw features is: " << time_used.count() << endl;
 
     Mat combined;
 
     //image 1 is on the left
     hconcat(image1, image2, combined);    
 
+     t1 = chrono::steady_clock::now();
+
     combined = drawMatches(matches, combined, image2.cols);
+
+     t2 = chrono::steady_clock::now();
+
+     time_used = chrono::duration_cast<chrono::duration<double>>(t2-t1);
+
+    cout << "Time to draw matches is: " << time_used.count() << endl;
+
+    chrono::steady_clock::time_point m2 = chrono::steady_clock::now();
+
+    chrono::duration<double> mtime_used = chrono::duration_cast<chrono::duration<double>>(m2-m1);
+
+    cout << "Total time is: " << mtime_used.count() << endl;
 
     imshow("s", combined);
 
